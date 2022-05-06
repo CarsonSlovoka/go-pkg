6dfadc51e2dc92ff079e3bc3aa264ab37183ef9a
Carson <jackparadise520a@gmail.com>
2022-05-06 13:40:45 +0800

    改傳入syncall.LazyProc減少每次呼叫的成本 (#3)

--- v2/syscall/winuser.go
+++ v2/syscall/winuser.go
@@ -5,28 +5,30 @@ import (
 	"unsafe"
 )
 
-func MessageBox(caption, body string, btnFlag uintptr) uintptr {
-	// https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-messageboxw
-	var user32DLL = syscall.NewLazyDLL("user32.dll")
-	var procMessageBox = user32DLL.NewProc("MessageBoxW") // Return value: Type int
+// MessageBoxW
+// https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-messageboxw
+func MessageBoxW(proc *syscall.LazyProc, caption, body string, btnFlag uintptr) uintptr {
+	if proc.Name != "MessageBoxW" {
+		panic(proc.Name != "MessageBoxW")
+	}
 	hwnd := uintptr(0x00)
 	lpCaption, _ := syscall.UTF16PtrFromString(caption) // LPCWSTR
 	lpText, _ := syscall.UTF16PtrFromString(body)       // LPCWSTR
-	responseVal, _, _ := procMessageBox.Call(hwnd,
+	responseVal, _, _ := proc.Call(hwnd,
 		uintptr(unsafe.Pointer(lpText)),
 		uintptr(unsafe.Pointer(lpCaption)),
 		btnFlag)
 	return responseVal
 }
 
-func MessageBoxOK(caption, body string) uintptr {
-	return MessageBox(caption, body, 0x00000000)
+func MessageBoxOK(proc *syscall.LazyProc, caption, body string) uintptr {
+	return MessageBoxW(proc, caption, body, 0x00000000)
 }
 
-func MessageBoxYesNo(caption, body string) uintptr {
-	return MessageBox(caption, body, 0x00000004)
+func MessageBoxYesNo(proc *syscall.LazyProc, caption, body string) uintptr {
+	return MessageBoxW(proc, caption, body, 0x00000004)
 }
 
-func MessageBoxYesNoCancel(caption, body string) uintptr {
-	return MessageBox(caption, body, 0x00000003)
+func MessageBoxYesNoCancel(proc *syscall.LazyProc, caption, body string) uintptr {
+	return MessageBoxW(proc, caption, body, 0x00000003)
 }
--- v2/syscall/winuser_test.go
+++ v2/syscall/winuser_test.go
@@ -3,11 +3,15 @@ package syscall
 import (
 	"fmt"
 	"github.com/CarsonSlovoka/go-pkg/v2/w32"
+	"syscall"
 	"testing"
 )
 
 func TestMessageBoxOK(t *testing.T) {
-	response := MessageBoxYesNo("Test", "Yes or No")
+	user32DLL := syscall.NewLazyDLL("user32.dll")
+	procMessageBox := user32DLL.NewProc("MessageBoxW")
+
+	response := MessageBoxYesNo(procMessageBox, "Test", "Yes or No")
 	switch response {
 	case w32.IDYES:
 		fmt.Println("Yes")
@@ -16,11 +20,12 @@ func TestMessageBoxOK(t *testing.T) {
 	default: // 基本上沒辦法有這個出現，對話框只有Yes,No可以選，右上角的打X也不能按
 		fmt.Println("Unknown")
 	}
-	_ = MessageBoxOK("Test", "OK")
-	_ = MessageBoxYesNoCancel("Test", "Yes No Cancel")
-	_ = MessageBox("Test", "Help button", w32.MB_HELP)
-	_ = MessageBox("Test", "OK CANCEL", w32.MB_OKCANCEL)
-	_ = MessageBox("Test", "ABORT RETRY IGNORE", w32.MB_ABORTRETRYIGNORE)
-	_ = MessageBox("Test", "RETRY CANCEL", w32.MB_RETRYCANCEL)
-	_ = MessageBox("Test", "CANCEL TRY CONTINUE", w32.MB_CANCELTRYCONTINUE)
+
+	_ = MessageBoxOK(procMessageBox, "Test", "OK")
+	_ = MessageBoxYesNoCancel(procMessageBox, "Test", "Yes No Cancel")
+	_ = MessageBoxW(procMessageBox, "Test", "Help button", w32.MB_HELP)
+	_ = MessageBoxW(procMessageBox, "Test", "OK CANCEL", w32.MB_OKCANCEL)
+	_ = MessageBoxW(procMessageBox, "Test", "ABORT RETRY IGNORE", w32.MB_ABORTRETRYIGNORE)
+	_ = MessageBoxW(procMessageBox, "Test", "RETRY CANCEL", w32.MB_RETRYCANCEL)
+	_ = MessageBoxW(procMessageBox, "Test", "CANCEL TRY CONTINUE", w32.MB_CANCELTRYCONTINUE)
 }
