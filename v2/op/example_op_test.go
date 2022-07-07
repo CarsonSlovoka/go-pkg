b0614f08825f8d866ed7226fff54e0311525f32e
Carson <jackparadise520a@gmail.com>
2022-07-07 16:00:14 +0800

    Add workflows/go_test.yaml (#8)

---
+++ v2/op/example_op_test.go
@@ -0,0 +1,17 @@
+package op_test
+
+import (
+	"fmt"
+	. "github.com/CarsonSlovoka/go-pkg/v2/op"
+)
+
+// 範例說明: Ternary
+func ExampleTernary() {
+	i := 30
+	fmt.Println(Ternary(i%2 == 0, "even", "odd").(string))
+	fmt.Println(Ternary(i > 30, 300, -1).(int))
+
+	// Output:
+	// even
+	// -1
+}
