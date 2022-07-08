15f5e381458475f86c55f72d33fa3a65944c0687
Carson <jackparadise520a@gmail.com>
2022-07-08 14:36:56 +0800

    http.DownloadFileWithTimeout (#3)

--- v2/net/http/download.go
+++ v2/net/http/download.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os"
 	"reflect"
+	"time"
 )
 
 // Download2Memory https://stackoverflow.com/a/21351456/9935654
@@ -26,20 +27,21 @@ func Download2Memory(url string) ([]byte, error) {
 
 // DownloadFile https://stackoverflow.com/a/33853856/9935654
 func DownloadFile[T *os.File | string](out T, url string) (err error) {
-	var outFile *os.File
-	if reflect.TypeOf(out).String() == "string" {
-		// Create the file
-		outFile, err = os.Create(fmt.Sprintf("%v", out))
-		if err != nil {
-			return err
-		}
-		defer outFile.Close()
+	return DownloadFileWithTimeout(out, url, 0)
+}
+
+func DownloadFileWithTimeout[T *os.File | string](out T, url string, timeout time.Duration) (err error) {
+	// Get the data
+
+	// resp, err := http.Get(url) // 使用這種方法沒辦法設定timeout
+	var client *http.Client
+	if timeout != 0 {
+		client = &http.Client{Timeout: timeout}
 	} else {
-		outFile = any(out).(*os.File)
+		client = &http.Client{}
 	}
 
-	// Get the data
-	resp, err := http.Get(url)
+	resp, err := client.Get(url)
 	if err != nil {
 		return err
 	}
@@ -48,6 +50,18 @@ func DownloadFile[T *os.File | string](out T, url string) (err error) {
 	// Check server response
 	if resp.StatusCode != http.StatusOK {
 		return fmt.Errorf("bad status: %s", resp.Status)
+	}
+
+	// Prepare the output file.
+	var outFile *os.File
+	if reflect.TypeOf(out).String() == "string" {
+		outFile, err = os.Create(fmt.Sprintf("%v", out))
+		if err != nil {
+			return err
+		}
+		defer outFile.Close()
+	} else {
+		outFile = any(out).(*os.File)
 	}
 
 	// Writer the body to file
--- v2/net/http/download_test.go
+++ v2/net/http/download_test.go
@@ -3,7 +3,9 @@ package http
 import (
 	"fmt"
 	"os"
+	"strings"
 	"testing"
+	"time"
 )
 
 func TestDownload2Memory(t *testing.T) {
@@ -12,6 +14,15 @@ func TestDownload2Memory(t *testing.T) {
 		t.Fatalf("%s", err)
 	}
 	_ = fmt.Sprintf("%s", dataBytes)
+}
+
+func ExampleDownload2Memory() {
+	dataBytes, err := Download2Memory("https://www.google.com.tw/")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(len(dataBytes) > 0)
+	// Output: true
 }
 
 func TestDownloadFile(t *testing.T) {
@@ -36,5 +47,47 @@ func TestDownloadFile(t *testing.T) {
 	}()
 	if err := DownloadFile(writer, "https://www.google.com.tw/"); err != nil {
 		t.Fatalf("%s", err)
+	}
+}
+
+func TestDownloadFileWithTimeout(t *testing.T) {
+	// We give it very little time to prepare, so a timeout error is bound to occur.
+	err := DownloadFileWithTimeout("temp.html", "https://www.google.com.tw/", time.Nanosecond)
+	if err == nil ||
+		!strings.Contains(err.Error(), "Timeout") { // https://github.com/golang/go/blob/1930977/src/net/http/client.go#L718-L723
+		t.Fatal("must error")
+	}
+}
+
+// Output support string or os.File both OK!
+func ExampleDownloadFile() {
+	// Output type: string
+	if err := DownloadFile("temp.html", "https://www.google.com.tw/"); err != nil {
+		panic(err)
+	}
+
+	// Output type: writer
+	writer, err := os.Create("temp2.html")
+	if err != nil {
+		panic(err)
+	}
+	if err = DownloadFile(writer, "https://www.google.com.tw/"); err != nil {
+		panic(err)
+	}
+}
+
+func ExampleDownloadFileWithTimeout() {
+	// Output type: string
+	if err := DownloadFileWithTimeout("temp.html", "https://www.google.com.tw/", 5*time.Second); err != nil {
+		panic(err)
+	}
+
+	// Output type: writer
+	writer, err := os.Create("temp2.html")
+	if err != nil {
+		panic(err)
+	}
+	if err = DownloadFileWithTimeout(writer, "https://www.google.com.tw/", 5*time.Second); err != nil {
+		panic(err)
 	}
 }
