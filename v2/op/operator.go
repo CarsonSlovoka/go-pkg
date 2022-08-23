dbf44b10587532521db646095091a839dcefafc7
Carson <jackparadise520a@gmail.com>
2022-08-23 13:20:59 +0800

    使用generic優化三元運算子 (#3)

--- v2/op/operator.go
+++ v2/op/operator.go
@@ -1,8 +1,18 @@
 package op
 
-func Ternary(statement bool, a, b interface{}) interface{} {
+// Deprecated: Use If instead of it.
+// 這種用法回傳的型別就只能any，還要透過斷言才有辦法變成所想要的型別
+func Ternary(statement bool, a, b any) any {
 	if statement {
 		return a
 	}
 	return b
+}
+
+// If 倚靠generic讓回傳的型別可與參數相同
+func If[T any](cond bool, trueVal, falseVal T) T {
+	if cond {
+		return trueVal
+	}
+	return falseVal
 }
--- v2/op/operator_test.go
+++ v2/op/operator_test.go
@@ -1,6 +1,9 @@
 package op
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestTernary(t *testing.T) {
 	i := 30
@@ -11,4 +14,24 @@ func TestTernary(t *testing.T) {
 	if Ternary(i > 30, 300, -1).(int) != -1 {
 		t.FailNow()
 	}
+}
+
+func ExampleIf() {
+	i := 30
+	fmt.Println(If(i >= 60, "OK", "Fail"))
+	i = 60
+	fmt.Println(If(i >= 60, 600, 0))
+
+	// 如果If沒有使用generic，這種用法會報錯: invalid operation: cannot call non-function msgFunc (variable of type any)
+	msgFunc := If(i >= 60, func() {
+		fmt.Println("123")
+	}, func() {
+		fmt.Println("Fail")
+	})
+	msgFunc()
+
+	// Output:
+	// Fail
+	// 600
+	// 123
 }
