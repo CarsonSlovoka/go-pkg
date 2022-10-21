aa88ae46615137ecb0cec2a3b233a23b3fe53c6d
Carson <jackparadise520a@gmail.com>
2022-10-21 15:57:40 +0800

    新增範例: 使用ExtractIcon來計算該檔案擁有的圖標數量 (#5)

--- v2/w32/shell_func_test.go
+++ v2/w32/shell_func_test.go
@@ -7,6 +7,7 @@ import (
 	"path/filepath"
 )
 
+// 使用ExtractIcon取得該應用程式的HICON
 func ExampleShellDLL_ExtractIcon() {
 	shell32dll := w32.NewShellDLL([]w32.ProcName{
 		w32.PNExtractIcon,
@@ -51,6 +52,55 @@ func ExampleShellDLL_ExtractIcon() {
 
 		if err := user32dll.DrawIcon(curHDC, 50, 100, hicon); err != nil {
 			panic(err)
+		}
+	}
+	// Output:
+}
+
+// 使用ExtractIcon來計算該檔案擁有的圖標數量
+func ExampleShellDLL_ExtractIcon_count() {
+	shell32dll := w32.NewShellDLL([]w32.ProcName{
+		w32.PNExtractIcon,
+	})
+
+	const exeFileName = "powershell.exe"
+
+	numIcon := shell32dll.ExtractIcon(0, exeFileName,
+		-1, // 抓出所有icon的數量
+	)
+
+	if numIcon == 0 {
+		return
+	}
+
+	// 作圖在notepad.exe上
+	{
+		user32dll := w32.NewUser32DLL([]w32.ProcName{
+			w32.PNFindWindow,
+			w32.PNGetDC,
+			w32.PNReleaseDC,
+			w32.PNDrawIcon,
+		})
+
+		hwndNotepad, err := user32dll.FindWindow("Notepad", "")
+		if err != nil {
+			log.Println("notepad.exe not found")
+			return
+		}
+		curHDC := user32dll.GetDC(hwndNotepad)
+		defer func() {
+			if curHDC != 0 {
+				if err = user32dll.ReleaseDC(hwndNotepad, curHDC); err != nil {
+					log.Fatal(err)
+				}
+			}
+		}()
+		for iconIdx := 0; iconIdx < int(numIcon); iconIdx++ {
+			hicon := shell32dll.ExtractIcon(0, exeFileName, iconIdx)
+
+			if err := user32dll.DrawIcon(curHDC, 50, 50*(iconIdx+1), hicon); err != nil {
+				panic(err)
+			}
 		}
 	}
 	// Output:
